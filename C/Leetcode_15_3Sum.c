058d5ec26eb7b394da08ccacd4ba1a835109fa5a
mjiang-27 <JMM2015@163.com>
2017-07-13 01:03:45 +0800

    Modify the codes.
    Modify the codes to run and get output. Remove duplicated array of results needs to be done.

--- C/Leetcode_15_3Sum.c
+++ C/Leetcode_15_3Sum.c
@@ -21,18 +21,19 @@ HashMap * hashMapConstruct(int hashMapSize)
 {
     // Malloc for HashMap
     HashMap *hashMap = (HashMap *)(malloc(sizeof(HashMap)));
-    
+
     // Assign for struct members
     hashMap->mHashMapSize = hashMapSize;
     hashMap->mVectors = (HashNode **)calloc(hashMapSize, sizeof(HashNode *));
-    
+
     return hashMap;
 }
 void hashMapDestroy(HashMap * hashMap)
 {
     // Free each dynamic pointer in each hash node
     HashNode *node = NULL;
-    for (int index = 0; index < hashMap->mHashMapSize; index++)
+    int index = 0;
+    for (; index < hashMap->mHashMapSize; index++)
     {
         node = hashMap->mVectors[index];
         if (node != NULL)
@@ -41,7 +42,7 @@ void hashMapDestroy(HashMap * hashMap)
             node = NULL;
         }
     }
-    
+
     // free dynamic pointer in hash map
     free(hashMap->mVectors);
     free(hashMap);
@@ -54,7 +55,7 @@ void hashMapSet(HashMap *hashMap, int key, int value)
 {
     int mode = abs(key % (hashMap->mHashMapSize));
     HashNode * newNode = NULL;
-    
+
     while ((newNode = hashMap->mVectors[mode]) != NULL)
     {
         if (mode < hashMap->mHashMapSize - 1)
@@ -66,12 +67,12 @@ void hashMapSet(HashMap *hashMap, int key, int value)
             mode = 0;
         }
     }
-    
+
     // Malloc for the new Hash Node
     newNode = (HashNode *)malloc(sizeof(HashNode));
     newNode->mKey = key;
     newNode->mValue = value;
-    
+
     // Add new Hash Node into Hash Map
     hashMap->mVectors[mode] = newNode;
 }
@@ -80,14 +81,14 @@ HashNode *hashMapGetByKey(HashMap *hashMap, int key)
 {
     int mode = abs(key % (hashMap->mHashMapSize));
     HashNode * newNode = NULL;
-    
+
     while ((newNode = hashMap->mVectors[mode]) != NULL)
     {
         if (newNode->mKey == key)
         {
             return newNode;
         }
-        
+
         if (mode < hashMap->mHashMapSize - 1)
         {
             mode++;
@@ -97,123 +98,143 @@ HashNode *hashMapGetByKey(HashMap *hashMap, int key)
             mode = 0;
         }
     }
-    
+
     return NULL;
 }
 
-int* twoSum(int* nums, int numsSize, int target)
+void swap(int *nums, int numsSize, int i, int j)
 {
-    if (nums == NULL || numsSize <= 0)
+    if (!nums || numsSize < 0 ||
+        i < 0 || i > numsSize ||
+        j < 0 || j > numsSize)
     {
-        return NULL;
+        return;
     }
-    
-    HashMap * hashMap = NULL;
-    HashNode * hashNode = NULL;
-    
-    // Construct a HashMap
-    hashMap = hashMapConstruct(numsSize);
-    if (hashMap == NULL)
-    {
-        return NULL;
-    }
-    
-    int diff = 0;
-    for (int index = 0; index < numsSize; index++)
-    {
-        diff = target - nums[index];
-        hashNode = hashMapGetByKey(hashMap, diff);
-        
-        if (hashNode != NULL)
-        {
-            int* result = malloc(sizeof(int)*2);
-            result[0] = hashNode->mValue;
-            result[1] = index;
-            
-            // Destroy the HashMap
-            hashMapDestroy(hashMap);
-            
-            return result;
-        }
-        else
-        {
-            hashMapSet(hashMap, nums[index], index);
-        }
-    }
-    
-    return NULL;
-}
 
-void swap(int *nums, int numsSize, int i, int j) 
-{
-	if (!nums || numsSize < 0 || 
-	    i < 0 || i > numsSize || 
-		j < 0 || j > numsSize) 
-	{
-		return;
-	}
-
-	int temp = nums[i];
-	nums[i] = nums[j];
-	nums[j] = temp;
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
 }
 
 // Core logics for quick sort algorithm used to sort an array
-int Partition(int *nums, int numsSize, int low, int high) 
+int Partition(int *nums, int numsSize, int low, int high)
 {
-	int pivotKey = nums[low];
-	
-	while (low < high) 
-	{
-		while (low < high && nums[high] >= pivotKey) 
-		{
-			high--;
-		}
-		swap(nums, numsSize, low, high);
-		
-		while (low < high && nums[low] <= pivotKey)) 
-		{
-			low++;
-		}
-		swap(nums, numsSize, low, high);
-	}
-	
-	return low;
+    int pivotKey = nums[low];
+
+    while (low < high)
+    {
+        while (low < high && nums[high] >= pivotKey)
+        {
+            high--;
+        }
+        swap(nums, numsSize, low, high);
+
+        while (low < high && nums[low] <= pivotKey)
+        {
+            low++;
+        }
+        swap(nums, numsSize, low, high);
+    }
+
+    return low;
 }
 
-void QuikSort(int *nums, int numsSize, int low, int high) 
+void QuickSort(int *nums, int numsSize, int low, int high)
 {
-	if (!nums || numsSize <= 0 || low > high) 
-	{
-		return;
-	}
+    int pivot;
 
-	int pivot;
-	
-	pivot = Partition(nums, numsSize, low, high);
-		
-	QuickSort(nums, numsSize, low, pivot - 1);
-	QuickSort(nums, numsSize, pivot, high);
+    if (low < high)
+    {
+        pivot = Partition(nums, numsSize, low, high);
+
+        QuickSort(nums, numsSize, low, pivot - 1);
+        QuickSort(nums, numsSize, pivot + 1, high);
+    }
+}
+
+// TODO: Deal with the duplicates
+void twoSum(int* nums, int lIndex, int rIndex, int target, int **result, int *resultCount)
+{
+    if (nums == NULL || rIndex <= lIndex || !result || !resultCount)
+    {
+        return;
+    }
+
+    HashMap * hashMap = NULL;
+    HashNode * hashNode = NULL;
+
+    // Construct a HashMap
+    hashMap = hashMapConstruct(rIndex - lIndex + 1);
+    if (hashMap == NULL)
+    {
+        return;
+    }
+
+    int diff = 0;
+    int index = lIndex;
+    for (; index <= rIndex; index++)
+    {
+        // Skip the duplicates
+        while (nums[index] == nums[index + 1])
+        {
+            index++;
+        }
+
+        diff = target - nums[index];
+        hashNode = hashMapGetByKey(hashMap, diff);
+
+        if (hashNode != NULL)
+        {
+            int* resElem = (int *)malloc(sizeof(int) * 3);
+            resElem[0] = -target;
+            resElem[1] = diff;
+            resElem[2] = nums[index];
+
+            result[*resultCount] = resElem;
+            (*resultCount)++;
+        }
+        
+        hashMapSet(hashMap, nums[index], index);
+
+    }
+
+    // Destroy the HashMap
+    hashMapDestroy(hashMap);
 }
 
 // Return an array of arrays of size *returnSize.
 // Note: The returned array must be malloced, assume caller calls free().
 int** threeSum(int* nums, int numsSize, int* returnSize) {
     // Validations
-	if (!nums || numsSize <= 2 || !returnSize) 
-	{
-		return NULL;
-	}
-	
-	// Sort the array
-	QuikSort(nums, numsSize, 0, numsSize - 1);
-	
-	// Traverse the sorted array and get array of result with 2 sum algorithm
-	int twoSumTarget;
-	for (int i = 0; i < numsSize; i++) 
-	{
-		twoSumTarget = -nums[i];
-		
-		// TODO
-	}
+    if (!nums || numsSize <= 2 || !returnSize)
+    {
+        return NULL;
+    }
+
+    // Sort the array
+    QuickSort(nums, numsSize, 0, numsSize - 1);
+
+    // Mallcate for result
+    int maxSize = numsSize * (numsSize - 1) * (numsSize - 2) / 6;
+    int** result = (int**)calloc(maxSize, sizeof(int*));
+
+    // Traverse the sorted array and get array of result with 2 sum algorithm
+    int resultCount = 0;
+    int i = 0;
+    for (; i < numsSize - 1; i++)
+    {
+        int twoSumTarget = -nums[i];
+
+        twoSum(nums, i + 1, numsSize - 1, twoSumTarget, result, &resultCount);
+
+        // Skip the duplicates
+        while (nums[i] == nums[i + 1])
+        {
+            i++;
+        }
+
+    }
+
+    *returnSize = resultCount;
+    return result;
 }
