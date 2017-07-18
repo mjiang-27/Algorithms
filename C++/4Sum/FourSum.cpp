c347146e5e75a0ce6adfba9dc019f6dc184ff2e4
mjiang-27 <JMM2015@163.com>
2017-07-17 23:50:44 -0700

    Update the solution with 3Sum. Make it easier to be understood. Note: Some optimizations left to be done later.

--- C++/4Sum/FourSum.cpp
+++ C++/4Sum/FourSum.cpp
@@ -4,10 +4,10 @@
 
 #include <algorithm>
 
-// TODO: solve the problem with 3Sum.
-// Refering: https://discuss.leetcode.com/topic/9712/4sum-c-solution-with-explanation-and-comparison-with-3sum-problem-easy-to-understand
+// This is much easier to understand comparing with the solution submitted yesterdy.
+// Using the solution of 3Sum.
 
-std::vector<std::vector<int>> Solution::fourSum(std::vector<int>& nums, int target)
+std::vector<std::vector<int>> Solution::fourSum_1(std::vector<int>& nums, int target)
 {
     std::vector<std::vector<int>> res;
 
@@ -79,6 +79,82 @@ std::vector<std::vector<int>> Solution::fourSum(std::vector<int>& nums, int targ
                     }
                 }
             }
+        }
+    }
+
+    return res;
+}
+
+std::vector<std::vector<int>> Solution::fourSum_2(std::vector<int>& nums, int target)
+{
+    std::vector<std::vector<int>> res;
+
+    int numsSize = nums.size();
+    if (numsSize < 4)
+    {
+        return res;
+    }
+
+    sort(nums.begin(), nums.end());
+
+    int firstIndex = 0;
+    for (; firstIndex < numsSize - 3; firstIndex++)
+    {
+        int secondIndex = firstIndex + 1;
+        for (; secondIndex < numsSize - 2; secondIndex++)
+        {
+            int thirdIndex = secondIndex + 1;
+            int forthIndex = numsSize - 1;
+            while (thirdIndex < forthIndex)
+            {
+                int sum = nums[firstIndex] + nums[secondIndex] + nums[thirdIndex] + nums[forthIndex];
+                if (sum < target)
+                {
+                    thirdIndex++;
+                }
+                else if (sum > target)
+                {
+                    forthIndex--;
+                }
+                else
+                {
+                    // Add a solution for the problem
+                    std::vector<int> elem;
+                    elem.push_back(nums[firstIndex]);
+                    elem.push_back(nums[secondIndex]);
+                    elem.push_back(nums[thirdIndex]);
+                    elem.push_back(nums[forthIndex]);
+                    res.push_back(elem);
+
+                    // Update the value for the third and forth index
+                    thirdIndex++;
+                    forthIndex--;
+
+                    // Skip duplicated integer for the third index
+                    while (thirdIndex < forthIndex && nums[thirdIndex] == nums[thirdIndex - 1])
+                    {
+                        thirdIndex++;
+                    }
+
+                    // Skip duplicated integer for the forth index
+                    while (thirdIndex < forthIndex && nums[forthIndex + 1] == nums[forthIndex])
+                    {
+                        forthIndex--;
+                    }
+                }
+            }
+
+            // Skip duplicated integer for the second index
+            while (secondIndex + 1 < numsSize - 2 && nums[secondIndex] == nums[secondIndex + 1])
+            {
+                secondIndex++;
+            }
+        }
+
+        // Skip duplicated integer for the first index
+        while (firstIndex + 1 < numsSize - 3 && nums[firstIndex] == nums[firstIndex + 1])
+        {
+            firstIndex++;
         }
     }
 
